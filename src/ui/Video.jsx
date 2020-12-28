a017c4b0a47f7cacf48cccfd1ee8f215e8f900ec
Emerson Silva <emerson_jdss@hotmail.com>
2020-12-28 14:14:23 -0300

    config video component

---
+++ src/ui/Video.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Video = ({videoObject, onClick}) => {
+    const video = videoObject || {};
+    // eslint-disable-next-line no-redeclare
+    const onClick = onClick || {};
+
+    return (
+        <li onClick={() => onClick(video)}>
+            <img src={video.cover} alt={video.title}/>
+            <span>{video.duration}</span>
+            <h2>{video.title}</h2>
+        </li>
+    )
+}
+
+export default Video;

e9b96f6ad208cc6ee7ea122e150990b506eb8161
Emerson Silva <emerson_jdss@hotmail.com>
2021-01-04 08:15:26 -0300

    config video component

--- src/App.js
+++ src/App.js
@@ -3,16 +3,14 @@ import "./index.css";
 import "./App.css";
 import VideoList from "./ui/VideoList";
 import NewVideoForm from "./ui/NewVideoForm";
-import UseEffectDefault from "./ui/UseEffectDefault";
-import UseRefComponent from "./ui/UseRefComponent";
+import VideoPlayer from "./ui/VideoPlayer";
 
 const App = () => {
   return (
     <Fragment>
-      {/* <VideoList /> */}
-      {/* <NewVideoForm /> */}
-      {/* <UseEffectDefault /> */}
-      <UseRefComponent />
+      <VideoList />
+      <VideoPlayer />
+      <NewVideoForm />
     </Fragment>
   );
 };

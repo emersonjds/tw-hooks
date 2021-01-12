a15cf6576aac4967162bab0b5ea748692efd0d98
Emerson Silva <emerson_jdss@hotmail.com>
2021-01-12 13:59:26 -0300

    config context

--- src/data/video/VideoContext.js
+++ src/data/video/VideoContext.js
@@ -1,34 +1,51 @@
 import React, { createContext } from "react";
 
-
 const _state = {
-	selectedVideo: {
-		url:
-			"https://cdn.videvo.net/videvo_files/video/premium/video0037/small_watermarked/docklands_clocks06_preview.webm",
-		duration: "10",
-		cover:
-			"https://media.istockphoto.com/photos/blurred-crowd-of-unrecognizable-at-the-street-picture-id1065178846",
-		title: "Video Name",
-	},
-	videoList: [
-		{
-			id: Math.random().toFixed(2) * 10,
-			title: "Video Name",
-			duration: "10",
-			cover: "https://media.istockphoto.com/photos/blurred-crowd-of-unrecognizable-at-the-street-picture-id1065178846",
-			url: "https://cdn.videvo.net/videvo_files/video/premium/video0037/small_watermarked/docklands_clocks06_preview.webm",
-		},
-	]
-}
+  selectedVideo: {
+    url:
+      "https://cdn.videvo.net/videvo_files/video/premium/video0037/small_watermarked/docklands_clocks06_preview.webm",
+    duration: "10",
+    cover:
+      "https://media.istockphoto.com/photos/blurred-crowd-of-unrecognizable-at-the-street-picture-id1065178846",
+    title: "Video Name",
+  },
+  videoList: [
+    {
+      id: Math.random().toFixed(2) * 10,
+      title: "Video Name",
+      duration: "10",
+      cover:
+        "https://media.istockphoto.com/photos/blurred-crowd-of-unrecognizable-at-the-street-picture-id1065178846",
+      url:
+        "https://cdn.videvo.net/videvo_files/video/premium/video0037/small_watermarked/docklands_clocks06_preview.webm",
+    },
+    {
+      id: Math.random().toFixed(2) * 10,
+      title: "Video Name",
+      duration: "10",
+      cover:
+        "https://media.istockphoto.com/photos/blurred-crowd-of-unrecognizable-at-the-street-picture-id1065178846",
+      url:
+        "https://cdn.videvo.net/videvo_files/video/premium/video0037/small_watermarked/docklands_clocks06_preview.webm",
+    },
+    {
+      id: Math.random().toFixed(2) * 10,
+      title: "Video Name",
+      duration: "10",
+      cover:
+        "https://media.istockphoto.com/photos/blurred-crowd-of-unrecognizable-at-the-street-picture-id1065178846",
+      url:
+        "https://cdn.videvo.net/videvo_files/video/premium/video0037/small_watermarked/docklands_clocks06_preview.webm",
+    },
+  ],
+};
 
 export const videoStore = createContext(_state);
 
-const {Provider} = videoStore;
+const { Provider } = videoStore;
 
-function VideoProvider({children}) {
-	return <Provider value={[_state]}>
-		{children}
-	</Provider>
+function VideoProvider({ children }) {
+  return <Provider value={[_state]}>{children}</Provider>;
 }
 
 export default VideoProvider;
